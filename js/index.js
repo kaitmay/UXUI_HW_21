efb95aad306ed8a54de6eb92b3b008a9f1fe456b
kaitmay <kait.brown23@gmail.com>
2022-02-16 19:43:41 -0500

    Created 3 pages with bootstrap

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log("your index.js file is loaded correctly");
+
+
+$('.carousel').carousel({
+  interval: 200
+});
+
+
+$("button").click(function(){
+  $("p").fadeIn();
+});
+
+$('.collapse').collapse()
+
